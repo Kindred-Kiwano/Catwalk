cd89cfb4edb1ee34f0ad922ff081de5b6d1b046c
Samuel Dudley <sbarkerdudley@gmail.com>
2021-10-27 22:41:46 -0700

    Card class stubbed out

---
+++ client/src/components/RelatedProducts/Card.jsx
@@ -0,0 +1,306 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let product = {
+  'id': 48451,
+  'campus': 'hr-sfo',
+  'name': 'Raymundo Skirt',
+  'slogan': 'Quibusdam magni sunt vitae distinctio ad id nihil vero ducimus.',
+  'description': 'Et est id repudiandae numquam incidunt nam. Consequatur iste fugiat asperiores quos et reiciendis laudantium. Debitis rem ipsum iste. Exercitationem quia explicabo quam. Consequatur nihil similique vitae nam rerum perspiciatis recusandae. Ut sint natus.',
+  'category': 'Skirt',
+  'default_price': '963.00',
+  'created_at': '2021-09-09T19:03:37.525Z',
+  'updated_at': '2021-09-09T19:03:37.525Z'
+};
+
+let styles = {
+  'product_id': '48437',
+  'results': [
+    {
+      'style_id': 293509,
+      'name': 'White',
+      'original_price': '89.00',
+      'sale_price': null,
+      'default?': true,
+      'photos': [
+        {
+          'thumbnail_url': 'https://images.unsplash.com/photo-1477420143023-6a0e0b04b69a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+          'url': 'https://images.unsplash.com/photo-1477420143023-6a0e0b04b69a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80'
+        },
+        {
+          'thumbnail_url': 'https://images.unsplash.com/photo-1518687338977-a84d3086a934?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+          'url': 'https://images.unsplash.com/photo-1518687338977-a84d3086a934?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=652&q=80'
+        },
+        {
+          'thumbnail_url': 'https://images.unsplash.com/photo-1546367791-e7447b431084?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+          'url': 'https://images.unsplash.com/photo-1546367791-e7447b431084?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80'
+        },
+        {
+          'thumbnail_url': 'https://images.unsplash.com/photo-1498168208808-4c2706938a2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+          'url': 'https://images.unsplash.com/photo-1498168208808-4c2706938a2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1001&q=80'
+        },
+        {
+          'thumbnail_url': 'https://images.unsplash.com/photo-1556812191-381c7e7d96d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+          'url': 'https://images.unsplash.com/photo-1556812191-381c7e7d96d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2982&q=80'
+        },
+        {
+          'thumbnail_url': 'https://images.unsplash.com/photo-1526330563440-3ae2174b6bce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+          'url': 'https://images.unsplash.com/photo-1526330563440-3ae2174b6bce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1655&q=80'
+        },
+        {
+          'thumbnail_url': 'uhttps://images.unsplash.com/photo-1515243061678-14fc18b93935?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+          'url': 'https://images.unsplash.com/photo-1515243061678-14fc18b93935?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80'
+        },
+        {
+          'thumbnail_url': 'https://images.unsplash.com/photo-1516199707327-5399434d0aa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+          'url': 'https://images.unsplash.com/photo-1516199707327-5399434d0aa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1083&q=80'
+        },
+        {
+          'thumbnail_url': 'https://images.unsplash.com/photo-1541444446610-749d3299b35e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+          'url': 'https://images.unsplash.com/photo-1541444446610-749d3299b35e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80'
+        },
+        {
+          'thumbnail_url': 'https://images.unsplash.com/photo-1548861216-17dd1ac80d5f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+          'url': 'https://images.unsplash.com/photo-1548861216-17dd1ac80d5f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=664&q=80'
+        },
+        {
+          'thumbnail_url': 'https://images.unsplash.com/photo-1558422504-3d17542c1799?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+          'url': 'https://images.unsplash.com/photo-1558422504-3d17542c1799?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80'
+        }
+      ],
+      'skus': {
+        '1702934': {
+          'quantity': 14,
+          'size': '7'
+        },
+        '1702935': {
+          'quantity': 25,
+          'size': '7.5'
+        },
+        '1702936': {
+          'quantity': 9,
+          'size': '8'
+        },
+        '1702937': {
+          'quantity': 2,
+          'size': '8.5'
+        },
+        '1702938': {
+          'quantity': 18,
+          'size': '9'
+        },
+        '1702939': {
+          'quantity': 12,
+          'size': '9.5'
+        },
+        '1702940': {
+          'quantity': 10,
+          'size': '10'
+        },
+        '1702941': {
+          'quantity': 18,
+          'size': '10.5'
+        },
+        '1702942': {
+          'quantity': 11,
+          'size': '11'
+        },
+        '1702943': {
+          'quantity': 35,
+          'size': '11.5'
+        },
+        '1702944': {
+          'quantity': 25,
+          'size': '12'
+        }
+      }
+    },
+    {
+      'style_id': 293510,
+      'name': 'Black',
+      'original_price': '89.00',
+      'sale_price': null,
+      'default?': false,
+      'photos': [
+        {
+          'thumbnail_url': 'https://images.unsplash.com/photo-1463100099107-aa0980c362e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+          'url': 'https://images.unsplash.com/photo-1463100099107-aa0980c362e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80'
+        },
+        {
+          'thumbnail_url': 'https://images.unsplash.com/photo-1536830220630-ce146cccac84?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80',
+          'url': 'https://images.unsplash.com/photo-1536830220630-ce146cccac84?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80'
+        },
+        {
+          'thumbnail_url': 'https://images.unsplash.com/photo-1459947727010-6267d2c1232f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+          'url': 'https://images.unsplash.com/photo-1459947727010-6267d2c1232f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80'
+        },
+        {
+          'thumbnail_url': 'https://images.unsplash.com/photo-1526113438757-122d6d54da4e?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80',
+          'url': 'https://images.unsplash.com/photo-1526113438757-122d6d54da4e?ixlib=rb-1.2.1&auto=format&fit=crop&w=1563&q=80'
+        },
+        {
+          'thumbnail_url': 'https://images.unsplash.com/photo-1523296357416-a4b3c4b9ee65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+          'url': 'https://images.unsplash.com/photo-1523296357416-a4b3c4b9ee65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80'
+        },
+        {
+          'thumbnail_url': 'https://images.unsplash.com/photo-1510867759970-3d2ca293be77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+          'url': 'https://images.unsplash.com/photo-1510867759970-3d2ca293be77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'
+        },
+        {
+          'thumbnail_url': 'https://images.unsplash.com/photo-1534960680480-ca9853707e10?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+          'url': 'https://images.unsplash.com/photo-1534960680480-ca9853707e10?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2384&q=80'
+        },
+        {
+          'thumbnail_url': 'https://images.unsplash.com/photo-1519330377309-9ee1c6783348?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
+          'url': 'https://images.unsplash.com/photo-1519330377309-9ee1c6783348?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'
+        },
+        {
+          'thumbnail_url': 'https://images.unsplash.com/11/converse-fields.jpg?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80',
+          'url': 'https://images.unsplash.com/11/converse-fields.jpg?ixlib=rb-1.2.1&auto=format&fit=crop&w=1570&q=80'
+        },
+        {
+          'thumbnail_url': 'https://images.unsplash.com/photo-1522032238811-74bc59578599?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80',
+          'url': 'https://images.unsplash.com/photo-1522032238811-74bc59578599?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80'
+        },
+        {
+          'thumbnail_url': 'https://images.unsplash.com/photo-1472186422470-1f3fbde547aa?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80',
+          'url': 'https://images.unsplash.com/photo-1472186422470-1f3fbde547aa?ixlib=rb-1.2.1&auto=format&fit=crop&w=1647&q=80'
+        }
+      ],
+      'skus': {
+        '1702945': {
+          'quantity': 14,
+          'size': '7'
+        },
+        '1702946': {
+          'quantity': 25,
+          'size': '7.5'
+        },
+        '1702947': {
+          'quantity': 9,
+          'size': '8'
+        },
+        '1702948': {
+          'quantity': 2,
+          'size': '8.5'
+        },
+        '1702949': {
+          'quantity': 18,
+          'size': '9'
+        },
+        '1702950': {
+          'quantity': 12,
+          'size': '9.5'
+        },
+        '1702951': {
+          'quantity': 10,
+          'size': '10'
+        },
+        '1702952': {
+          'quantity': 18,
+          'size': '10.5'
+        },
+        '1702953': {
+          'quantity': 11,
+          'size': '11'
+        },
+        '1702954': {
+          'quantity': 35,
+          'size': '11.5'
+        },
+        '1702955': {
+          'quantity': 25,
+          'size': '12'
+        }
+      }
+    }
+  ]
+};
+
+/*
+
+types/styles: {
+
+  outfit: {
+    state: {
+      selected: bool,
+      onSale: bool
+    },
+    button: {
+      add (if !selected),
+      del (if selected/in Outfit carousel)
+    },
+    methods: {
+      remove: (obj, list) => {
+        splice global state outfit array
+      }
+    }
+  },
+  related: {
+    state: none
+    button: {
+      star,
+      add (if !selected)
+    },
+    methods: {
+      add: (obj, list) => {
+        push to global state Outfit array
+        then remove(obj, list) -> from related products global state array
+      }
+    }
+  }
+}
+
+*/
+
+const Rating = () => (
+  <div> * * * * * </div>
+);
+
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sale: false,
+      active: false,
+      type: ['OUTFIT', 'RELATED'],
+      styles: 'this.props.results.length'
+    };
+  }
+
+  add(id, list) {
+    // update global state for corresponding list
+  }
+
+  remove(id, list) {
+    // splice global state for corresponding list
+  }
+
+  render() {
+    // eslint-disable-next-line camelcase
+    let { id, name, original_price, sale_price, style } = this.props;
+    // eslint-disable-next-line camelcase
+    if (sale_price) {
+      //change CSS, display two prices
+    }
+
+    return (
+
+      <div className={'card-outfit/card-related'} id={'this.props.id'}>
+        <span className='header'>
+          <img src={''} classNaem={'thumbnail-card'}/>
+          <button className={'this.props.button.type'}></button>
+        </span>
+        <span className={'desc'}>
+          <div className='category'> {'category'} </div>
+          <div className='name' id='id'> {'name'} </div>
+          <div className='price' sale={'true/false'} range={['min', 'max']}> {'price'} </div>
+          <Rating product_id={'id'} type={'OUTFIT'}/>
+        </span>
+      </div>
+    );
+  }
+}
+
+export default Card;
+
---
+++ client/src/components/RelatedProducts/Carousel.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="carousel">
+        {/* For each item in list, render a card Component */}
+      </div>
+    );
+  }
+}
+
+export default Carousel;
