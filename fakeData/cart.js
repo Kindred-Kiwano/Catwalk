2ad17d6e21cf0dccd5e682d6683c4a2b0948d357
Samuel Dudley <sbarkerdudley@gmail.com>
2021-10-27 19:54:11 -0700

    Static data from most, but not all, API GET endpoints - Exported as an object, i.e. let {info, related} = Product

---
+++ fakeData/cart.js
@@ -0,0 +1,16 @@
+var cart = [
+  {
+    'sku_id': 1,
+    'count': 2
+  },
+  {
+    'sku_id': 3,
+    'count': 1
+  },
+  {
+    'sku_id': 5,
+    'count': 33
+  },
+];
+
+export default cart;
---
+++ fakeData/reviews.js
@@ -0,0 +1,127 @@
+var Reviews = {};
+
+var reviews = {
+  'product': '48432',
+  'page': 0,
+  'count': 5,
+  'results': [
+    {
+      'review_id': 841662,
+      'rating': 2,
+      'summary': 'Not good value',
+      'recommend': false,
+      'response': null,
+      'body': 'Wouldn\'t recommend. The quality is on par with cheaper competitors. This was so bad that I tried to call the company when I got my order. The customer service rep I spoke to only answered when I called him on his private line. He was polite and said I could get a refund but it\'s still poor value for money.',
+      'date': '2021-09-27T00:00:00.000Z',
+      'reviewer_name': 'Elizabeth',
+      'helpfulness': 0,
+      'photos': [
+        {
+          'id': 1595543,
+          'url': 'https://images.unsplash.com/photo-1544376664-80b17f09d399?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1525&q=80'
+        },
+        {
+          'id': 1595542,
+          'url': 'https://images.unsplash.com/photo-1544376664-80b17f09d399?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1525&q=80'
+        }
+      ]
+    },
+    {
+      'review_id': 841661,
+      'rating': 2,
+      'summary': 'Not good value',
+      'recommend': false,
+      'response': null,
+      'body': 'Wouldn\'t recommend. The quality is on par with cheaper competitors. This was so bad that I tried to call the company when I got my order. The customer service rep I spoke to only answered when I called him on his private line. He was polite and said I could get a refund but it\'s still poor value for money.',
+      'date': '2021-09-27T00:00:00.000Z',
+      'reviewer_name': 'Elizabeth',
+      'helpfulness': 0,
+      'photos': [
+        {
+          'id': 1595540,
+          'url': 'https://images.unsplash.com/photo-1544376664-80b17f09d399?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1525&q=80'
+        },
+        {
+          'id': 1595541,
+          'url': 'https://images.unsplash.com/photo-1544376664-80b17f09d399?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1525&q=80'
+        }
+      ]
+    },
+    {
+      'review_id': 1055452,
+      'rating': 3,
+      'summary': 'Good product',
+      'recommend': true,
+      'response': null,
+      'body': 'I liked the product',
+      'date': '2021-10-27T00:00:00.000Z',
+      'reviewer_name': 'AdamJ',
+      'helpfulness': 0,
+      'photos': [
+        {
+          'id': 2022587,
+          'url': 'https://media.baltzarcontent.com/uploads/2018/09/Schneiders-Forest-Green-Loden-Wool-Coat-Feature.png'
+        }
+      ]
+    },
+    {
+      'review_id': 1055451,
+      'rating': 5,
+      'summary': 'Great product',
+      'recommend': true,
+      'response': null,
+      'body': 'I really loved this product and appreaciated its quality',
+      'date': '2021-10-27T00:00:00.000Z',
+      'reviewer_name': 'adamjohnson',
+      'helpfulness': 0,
+      'photos': []
+    },
+    {
+      'review_id': 1055450,
+      'rating': 5,
+      'summary': 'Great product',
+      'recommend': true,
+      'response': null,
+      'body': 'I really loved this product and appreaciated its quality',
+      'date': '2021-10-27T00:00:00.000Z',
+      'reviewer_name': 'adamjohnson',
+      'helpfulness': 0,
+      'photos': []
+    }
+  ]
+};
+
+Reviews.meta = {
+  'product_id': '48432',
+  'ratings': {
+    '1': '42',
+    '2': '36',
+    '3': '12',
+    '4': '7',
+    '5': '93'
+  },
+  'recommended': {
+    'false': '128',
+    'true': '62'
+  },
+  'characteristics': {
+    'Fit': {
+      'id': 162510,
+      'value': '2.2960000000000000'
+    },
+    'Length': {
+      'id': 162511,
+      'value': '2.4577464788732394'
+    },
+    'Comfort': {
+      'id': 162512,
+      'value': '2.1971830985915493'
+    },
+    'Quality': {
+      'id': 162513,
+      'value': '2.2338709677419355'
+    }
+  }
+};
+
+export default Reviews;
