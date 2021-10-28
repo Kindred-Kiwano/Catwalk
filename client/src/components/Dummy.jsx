69c80e97c77c9d6076c3b2af7adc4757365e3e86
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-10-28 15:00:03 -0500

    Built server for static files, moved index.jsx out of components folder, added Dummy.jsx

---
+++ client/src/components/Dummy.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var Dummy = (props) => {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <p>webpack is working well oWo</p>
+
+    </div>
+  );
+};
+
+export default Dummy;
---
+++ client/src/index.jsx
@@ -0,0 +1,7 @@
+// babel should compile anything imported here and anything imported to those files
+
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Dummy from './components/Dummy.jsx';
+
+ReactDOM.render(<Dummy/>, document.getElementById('app'));
