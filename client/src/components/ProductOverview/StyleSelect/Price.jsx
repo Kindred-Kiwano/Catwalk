eb0f5bdf15f623d0084eea0631242e6ae08b0362
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-11-01 18:41:08 -0500

    Functionality added to discount vs. no discount

--- client/src/components/ProductOverview/StyleSelect/Price.jsx
+++ client/src/components/ProductOverview/StyleSelect/Price.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import { FakeProduct } from 'ProductOverview.jsx';
 
 var Price = () => {
+  var prod = React.useContext(FakeProduct);
   return (
     <div>
-      Area to display price based on selected style
+      Price: $ {getPrice(prod)}
+
     </div>
   );
 };
+
+
+var getPrice = (prod) => {
+  // MAKE DYNAMIC WHEN GETTING FROM API
+  var regular = prod.styles.results[0].original_price;
+  var discounted = prod.styles.results[0].sale_price;
+
+  // if there's a discounted price,
+  // display the discount price followed by the struckthru original price
+  if (prod.styles.results[0].sale_price === null) {
+    // display original price
+    return <span>{regular}</span>;
+  } else {
+    return (
+      <span>
+        <span>{discounted}</span>
+        <span className="obnoxious-discount">{regular}</span>
+      </span>
+    );
+  }
+
+};
+
 
 export default Price;
