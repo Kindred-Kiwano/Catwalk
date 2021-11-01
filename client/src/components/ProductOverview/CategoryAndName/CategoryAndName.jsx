6d97ca7a305407ac248471d27c5026bf5ef2a3f1
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-11-01 14:24:39 -0500

    Reformatted product titleReformatted product titleReformatted product titleReformatted product titleReformatted product titleReformatted product titleReformatted product titleReformatted product titleReformatted product title

--- client/src/components/ProductOverview/CategoryAndName/CategoryAndName.jsx
+++ client/src/components/ProductOverview/CategoryAndName/CategoryAndName.jsx
@@ -7,8 +7,7 @@ var CategoryAndName = () => {
   return (
     <div>
       <h4>CATEGORY AND NAME SUBCOMPONENT</h4>
-      <a href="https://shop.lululemon.com/c/mens-polos/_/N-8kx" >{prod.info.name}</a>
-      <span> {'>'} super fashionable</span>
+      <p>{ `${prod.info.category} > ${prod.info.name}` }</p>
     </div>
   );
 
