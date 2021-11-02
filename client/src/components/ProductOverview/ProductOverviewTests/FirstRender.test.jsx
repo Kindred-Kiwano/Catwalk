60a4b66c02f9fdd88cef63d884dc8d7411cad900
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-11-01 21:57:32 -0500

    Figuring out jest bug on import statement without "from"

--- client/src/components/ProductOverview/ProductOverviewTests/FirstRender.test.jsx
+++
@@ -1,9 +0,0 @@
-// test suite
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import ProductOverview from '../ProductOverview.jsx';
-
-test('should add two numbers', () => {
-  expect((1 + 2)).toBe(3);
-});
-
--- client/src/components/ProductOverview/Tests/ProductOverview.test.jsx
+++ client/src/components/ProductOverview/Tests/ProductOverview.test.jsx
@@ -1,7 +1,7 @@
 // test suite
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import ProductOverview from './ProductOverview.js';
+import ProductOverview from '../ProductOverview.jsx';
 
 test('should add two numbers', () => {
   expect((1 + 2)).toBe(3);
