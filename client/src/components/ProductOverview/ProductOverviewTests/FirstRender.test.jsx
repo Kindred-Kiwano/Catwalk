61c3ed38d50a2818965dce0d8b8455883fa8fae5
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-11-01 20:46:37 -0500

    Changed collectCoverage to false

--- client/src/components/ProductOverview/ProductOverviewTests/FirstRender.test.jsx
+++ client/src/components/ProductOverview/ProductOverviewTests/FirstRender.test.jsx
@@ -1,10 +1,9 @@
 // test suite
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import ProductOverview from './ProductOverview.js';
+import ProductOverview from '../ProductOverview.jsx';
 
 test('should add two numbers', () => {
   expect((1 + 2)).toBe(3);
 });
 
-// this is buggy, find required packages for jest or change test libraries
