d1b8c894f9a2d991c3a358dbbca800ee823aa88c
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-11-02 00:48:35 -0500

    Made and passed first test suite

--- client/src/components/ProductOverview/Tests/ProductOverview.test.jsx
+++ client/src/components/ProductOverview/Tests/ProductOverview.test.jsx
@@ -1,10 +1,28 @@
 // test suite
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import ProductOverview from '../ProductOverview.jsx';
+import { ProductOverview } from '../ProductOverview.jsx';
 
-test('should add two numbers', () => {
-  expect((1 + 2)).toBe(3);
+describe('Basic content rendering', () => {
+
+  test('should render text to the page', () => {
+    render(<ProductOverview />);
+    var priceMentions = screen.findAllByText(/price/i).then((array) => {
+      expect(array.length).not.toBe(0);
+    }).catch((err) => {
+      throw err;
+    });
+  });
+
+  test('should render images to the page', () => {
+    render(<ProductOverview />);
+    var images = screen.findAllByRole('image');
+    expect(images.length).not.toBe(0);
+  });
+
+  // to test:
+    // import and render <Gallery/> make sure only 7 images render to the carousel at a time (need to actually fix that)
+    // test components that take props - pass in props in the render() method to see if they appear in the expect block
 });
 
-// this is buggy, find required packages for jest or change test libraries
+
