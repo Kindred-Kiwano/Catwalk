09bb05be11c87ebc8b2830d9f2d4d9714f7329ae
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-11-03 18:47:58 -0500

    Fixed image-change bug on new style selection

--- client/src/components/ProductOverview/StyleSelect/Thumbnail.jsx
+++ client/src/components/ProductOverview/StyleSelect/Thumbnail.jsx
@@ -5,12 +5,13 @@ import { CurrentlySelectedImage } from './StyleSelect.jsx';
 
 var Thumbnail = (props) => {
   var [style, updateStyle] = React.useContext(Style);
-  console.log('the currently selected image is: ', React.useContext(CurrentlySelectedImage));
+  var [selected, updateSelected] = React.useContext(CurrentlySelectedImage);
 
   var changeStyle = (event) => {
     // using props.styleObject to get reference to the item clicked (change later if you factor this function into another file)
     if (style.style_id !== props.styleObject.style_id) {
       updateStyle(props.styleObject);
+      updateSelected(props.styleObject.photos[0]);
       console.log('updated to: ', props.styleObject.name);
     }
   };
