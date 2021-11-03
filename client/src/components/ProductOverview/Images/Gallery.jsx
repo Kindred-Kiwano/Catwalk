fdcc3a69e5173c350d5ab98e96faf0daa04e1cc3
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-11-02 21:31:41 -0500

    Wired gallery photos to click handlers

--- client/src/components/ProductOverview/Images/Gallery.jsx
+++ client/src/components/ProductOverview/Images/Gallery.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { FakeProduct } from '../ProductOverview.jsx';
 import GalleryImage from './GalleryImage.jsx';
 import { ImagesOfSelectedStyle } from './Images.jsx';
+import getURL from '../methods/getURL.js';
 
 var Gallery = () => {
   var [imageGallery, updateImageGallery] = React.useContext(ImagesOfSelectedStyle);
+
   return (
     <div>
       {imageGallery.map((imageObj) => {
-        return <GalleryImage thumbnailURL={imageObj.thumbnail_url} />;
+        return <GalleryImage thumbnailURL={imageObj.thumbnail_url} fullImageURL={imageObj.url} />;
       })}
     </div>
   );
--- client/src/components/ProductOverview/Images/GalleryImage.jsx
+++ client/src/components/ProductOverview/Images/GalleryImage.jsx
@@ -2,17 +2,12 @@ import React from 'react';
 import '../styles.css';
 import { CurrentlySelectedImage } from './Images.jsx';
 
-
 var GalleryImage = (props) => {
   // move this referece and function to another file to save on memory?
   var [selected, updateSelected] = React.useContext(CurrentlySelectedImage);
   var selectNewImage = (event) => {
-    var url = event.target.src;
-    // left off here, need to make a function that searches for the object containing the current url,
-      // then grabbing the full-res image to set state with
-
-
-    // get the larger image associated with that thumbnail
+    // send the other URL we have here in props to be rendered as the full image
+    updateSelected(props.fullImageURL);
     // use CurrentlySelectedImage's context value to access the function that updates the selected image state
   };
 
