b5cdd3c1de8431de54c51a042504aa658d33ce70
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-11-04 00:32:24 -0500

    Troubleshooting infinite loop of re-rendering, may need to look into using another argument to useEffect

--- client/src/components/ProductOverview/ProductOverview.jsx
+++ client/src/components/ProductOverview/ProductOverview.jsx
@@ -12,22 +12,26 @@ import { elliotsGetAllProducts, getAllStyles } from '../../../../Shared/makeRequ
 export var FakeProduct = React.createContext();
 export var Style = React.createContext();
 
+// product that the page refers to
 export var ProductOverview = () => {
-  // product that the page refers to
+
 
   // -- LEFT OFF HERE --
-    // need to call to useEffect so that it can update the state values
-      // may be a problem doing this if the browser renders all components with no context value until the re-render once we complete the network requests
+  // don't open the page, it'll cause an infinite loop!
+    // only want to rerender when we don't have all info to continue
+    // once we have both the product and the styles, and ONLY then,
+      // render the rest of the app
 
 
 
   // will give this .info and .styles just like the dummy data
   var Product = {};
-  // now we make a networdk request to update the product
-  if (Product.info && Product.styles) {
-    var [product, updateProduct] = React.useState(Product.info);
-    var [style, updateStyle] = React.useState(Product.styles[0]);
+  var [product, updateProduct] = React.useState(null);
+  var [style, updateStyle] = React.useState(null);
 
+
+  if (Product.info !== undefined && Product.styles !== undefined) {
+    // render through
     return (
       <div>
         <FakeProduct.Provider value={[product, updateProduct]}>
@@ -41,6 +45,26 @@ export var ProductOverview = () => {
       </div>
     );
 
+  } else if (Product.info === undefined && Product.styles !== undefined) {
+    // get styles of first product
+    getAllStyles(Product.info.id)
+      .then((stylesResponse) => {
+        Product.styles = stylesResponse;
+        // rerender
+        updateStyle(Product.styles[0]);
+      })
+      .catch((err) => {
+        console.log(`error getting styles for product "${Product.info.name}"`);
+        throw err;
+      });
+
+    return (
+      <p>
+        Loading product details...
+      </p>
+    );
+
+
   } else {
   // make network request to get 5 products
     elliotsGetAllProducts()
@@ -48,17 +72,8 @@ export var ProductOverview = () => {
         console.log('got a response!', productsResponse.data[0].id);
         // for now arbitrarilly choosing the first product
         Product.info = productsResponse.data[0];
-
-        getAllStyles(Product.info.id)
-          .then((stylesResponse) => {
-            Product.styles = stylesResponse;
-            // force rerender
-
-          })
-          .catch((err) => {
-            console.log(`error getting styles for product "${Product.info.name}"`);
-            throw err;
-          });
+        // rerender
+        updateProduct(Product.info);
       })
       .catch((err) => {
         console.log('error getting products');
