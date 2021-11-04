e6ac5f71c0d6a5ced22482860c6afcb0f4a3551e
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-11-03 23:51:06 -0500

    Having problems rerendering, may refactor to class-based

--- client/src/components/ProductOverview/ProductOverview.jsx
+++ client/src/components/ProductOverview/ProductOverview.jsx
@@ -19,50 +19,58 @@ export var ProductOverview = () => {
     // need to call to useEffect so that it can update the state values
       // may be a problem doing this if the browser renders all components with no context value until the re-render once we complete the network requests
 
-  // updateProduct in call to useEffect once we get back the product info
-  var [product, updateProduct] = React.useState();
 
-  // updateStyle above in call to useEffect once we get the product styles :)
-  var [style, updateStyle] = React.useState();
 
   // will give this .info and .styles just like the dummy data
   var Product = {};
   // now we make a networdk request to update the product
-  React.useEffect(() => {
-    // make network request to get 5 products
+  if (Product.info && Product.styles) {
+    var [product, updateProduct] = React.useState(Product.info);
+    var [style, updateStyle] = React.useState(Product.styles[0]);
+
+    return (
+      <div>
+        <FakeProduct.Provider value={[product, updateProduct]}>
+          <Style.Provider value={[style, updateStyle]} >
+            <CategoryAndName />
+            <ProductInfo />
+            <Share />
+            <StyleSelect />
+          </Style.Provider>
+        </FakeProduct.Provider>
+      </div>
+    );
+
+  } else {
+  // make network request to get 5 products
     elliotsGetAllProducts()
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((err) => {
-      console.log('got an error fetching from API');
-      throw err;
-    });
-    // then:
-      // use the resulting product id of the first one
-      // give response to Product.info
-    // then:
-      // then make a request to get the styles of that product
-      // give response to Product.styles
-      // updateProduct(complete Product object)
+      .then((productsResponse) => {
+        console.log('got a response!', productsResponse.data[0].id);
+        // for now arbitrarilly choosing the first product
+        Product.info = productsResponse.data[0];
 
+        getAllStyles(Product.info.id)
+          .then((stylesResponse) => {
+            Product.styles = stylesResponse;
+            // force rerender
 
-  });
+          })
+          .catch((err) => {
+            console.log(`error getting styles for product "${Product.info.name}"`);
+            throw err;
+          });
+      })
+      .catch((err) => {
+        console.log('error getting products');
+        throw err;
+      });
 
+    return (
+      <p>
+        Loading product details...
+      </p>
+    );
+  }
 
-
-
-  return (
-    <div>
-      <FakeProduct.Provider value={[product, updateProduct]}>
-        <Style.Provider value={[style, updateStyle]} >
-          <CategoryAndName />
-          <ProductInfo />
-          <Share />
-          <StyleSelect />
-        </Style.Provider>
-      </FakeProduct.Provider>
-    </div>
-  );
 };
 
