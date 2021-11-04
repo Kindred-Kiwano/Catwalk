a44074664371b54e20589a586542be1dd9836bf5
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-11-04 16:53:32 -0500

    Refactored arrow button event handlers into one function

--- client/src/components/ProductOverview/StyleSelect/Images/Carousel.jsx
+++ client/src/components/ProductOverview/StyleSelect/Images/Carousel.jsx
@@ -8,32 +8,25 @@ var Carousel = () => {
   var [imageGallery, updateImageGallery] = React.useContext(ImagesOfSelectedStyle);
   var [selected, updateSelected] = React.useContext(CurrentlySelectedImage);
 
-  var selectLeft = ( ) => {
-    var nextTargetIndex = selected.index - 1;
-    if (nextTargetIndex >= 0) {
-      var gotToPhoto = imageGallery[nextTargetIndex];
-      updateSelected(gotToPhoto);
+  var moveSelection = (event) => {
+    var nextTargetIndex;
+    if (event.target.innerHTML === '&lt;') {
+      nextTargetIndex = selected.index - 1;
+    } else {
+      nextTargetIndex = selected.index + 1;
+    }
+
+    if (nextTargetIndex >= 0 && nextTargetIndex < imageGallery.length) {
+      var goToPhoto = imageGallery[nextTargetIndex];
+      updateSelected(goToPhoto);
     }
   };
-
-  var selectRight = ( ) => {
-    var nextTargetIndex = selected.index + 1;
-    if (nextTargetIndex < imageGallery.length) {
-      var gotToPhoto = imageGallery[nextTargetIndex];
-      updateSelected(gotToPhoto);
-    }
-  };
-
-
-
-  console.log('selected image: ', selected);
-  console.log('image gallery: ', imageGallery);
 
   return (
     <div>
-      <button onClick={selectLeft}>{'<'} prev</button>
+      <button onClick={moveSelection}>{'<'}</button>
       <Gallery />
-      <button onClick={selectRight}>next {'>'}</button>
+      <button onClick={moveSelection}>{'>'}</button>
     </div>
   );
 };
