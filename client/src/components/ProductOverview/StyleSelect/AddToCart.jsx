4d9438eb51e957c4ac5d014ecaac9ce6406937bb
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-11-05 15:36:55 -0500

    Looked for solutions to opening dropdown menu automatically, return to this later

--- client/src/components/ProductOverview/StyleSelect/AddToCart.jsx
+++ client/src/components/ProductOverview/StyleSelect/AddToCart.jsx
@@ -7,9 +7,7 @@ var AddToCart = (props) => {
 
   var validateAddToCart = (event) => {
     // dont worry about if the item is out of stock - the button will be disabled and this function won't fire
-    // if the selected state is "Select size"
     if (props.sizeSelected === 'Select size') {
-      // add to the paragraph some reminder text
       document.getElementById('reminder-to-select-size').innerHTML = 'Please select a size';
     } else {
       alert('added to cart!');
@@ -18,7 +16,7 @@ var AddToCart = (props) => {
 
   return (
     <div>
-      {/* TODO project requirement: add a reminder to select size if user hasn't done so (use the <p> in the SelectSize.jsx) and open the SelectSize dropdown */}
+      {/* TODO project requirement: when reminding user to select a size on clicking add to cart, open the dropdown menu. *** Couldn't yet figure this out, come back to it later *** */}
       {/* TODO project requirement: hide button if selected style is out of stock */}
       {/* TODO project requirement: if both size and qty are valid, clicking the button should add the item(s) to the user's cart */}
 
