c707e61d98a4a59bcaa3bbf747192a4c49563cdc
Samuel Dudley <sbarkerdudley@gmail.com>
2021-11-06 03:00:12 -0700

    Server routes implemented -

--- server/routes/related.js
+++ server/routes/related.js
@@ -3,29 +3,35 @@ const router = express.Router();
 
 const {
   getRelatedIds,
-  getStyle,
-  getStyles,
   getProductData,
-  formatStylesData,
-  getFeatures
-} = require('Related');
+  getStyles,
+  formatStyle,
+  formatStyles,
+} = require('../../Shared/related.js');
 
+router.get('/', (req, res) => {
+  try {
+    res.status(200).send('fix me');
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.get('/related/:id', async (req, res) => {
+  let {id} = req.params;
+  try {
+    const ids = await getRelatedIds(id);
+    res.status(200).send(ids.data);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
 router.get('/:id', async (req, res) => {
   let {id} = req.params;
   try {
-    const ids = await getRelatedIds(id)
-    res.send(ids.data);
-  } catch (error) {
-    res.send(error);
-  }
-});
-
-router.get('/features/:id', async (req, res) => {
-  let {id} = req.params;
-  try {
-    let features = await getFeatures(id);
-    res.send(features.data);
+    let product = await getProductData(id);
+    res.status(200).send(product.data); //works
   } catch (error) {
     res.status(400).send(error);
   }
@@ -35,10 +41,11 @@ router.get('/styles/:id', async (req, res) => {
   let {id} = req.params;
   try {
     let styles = await getStyles(id)
-    res.send(styles.data);
+      .then(styles => formatStyles(styles.data))
+      .then(array => res.status(200).send(array));
   } catch (error) {
     res.status(400).send(error);
   }
-});
+}); //working ! sends an array of formatted objects, to be extended with basic data
 
 module.exports = router;
