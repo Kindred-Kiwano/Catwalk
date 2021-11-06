7539527a2c075c5fd3741f8cd539e1509bacc440
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-11-05 19:52:54 -0500

    Adjusted selected image of new style to retain index of previosly selected

--- client/src/components/ProductOverview/StyleSelect/Thumbnail.jsx
+++ client/src/components/ProductOverview/StyleSelect/Thumbnail.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 import '../styles.css';
 import { Style } from '../ProductOverview.jsx';
 import { CurrentlySelectedImage } from './StyleSelect.jsx';
+import { ImagesOfSelectedStyle } from './StyleSelect.jsx';
 
 var Thumbnail = (props) => {
   var [style, updateStyle] = React.useContext(Style);
   var [selected, updateSelected] = React.useContext(CurrentlySelectedImage);
+  var [imageGallery, updateImageGallery] = React.useContext(ImagesOfSelectedStyle);
 
   var changeStyle = (event) => {
     // using props.styleObject to get reference to the item clicked (change later if you factor this function into another file)
     if (style.style_id !== props.styleObject.style_id) {
       updateStyle(props.styleObject);
-      updateSelected(props.styleObject.photos[0]);
+      // now image gallery is of the newly selected style
+      var newSelected = props.styleObject.photos[selected.index];
+      if (!newSelected) {
+        // if overindexed,
+        // just give it the last space in the gallery
+        newSelected = props.styleObject.photos[imageGallery.length - 1];
+      }
+      updateSelected(newSelected);
       console.log('updated to: ', props.styleObject.name);
     }
   };
