3bb7fbd52614bfe542adac792c6490e6bbfa0b91
Samuel Dudley <sbarkerdudley@gmail.com>
2021-11-08 10:34:31 -0800

    Server side routes and templating updated

--- Shared/related.js
+++ Shared/related.js
@@ -2,12 +2,21 @@ const axios = require('../config/config.js');
 const $ = require('jquery');
 
 module.exports = {
+  getProducts: (params) => {
+    params =
+      params || {
+        count: 5,
+        page: 1
+      }
+
+    return axios.get(`/products`, params);
+  },
 
   getRelatedIds: (id) => {
     return axios.get(`/products/${id}/related`);
   },
 
-  getProductData: (id) => {
+  getProductInfo: (id) => {
     return axios.get(`/products/${id}`);
   },
 
@@ -15,48 +24,73 @@ module.exports = {
     return axios.get(`/products/${id}/styles`);
   },
 
-  formatStyle: (prod) => {
-    let {results, product_id} = prod;
-    let p = results[0];
-    let {
-      sale_price,
-      original_price,
-      photos
-    } = p;
+  formatFeatures: (array) => array.map(f => [f.feature].push(f.value ? [f.value] : null)),
+
+  formatInfo:  ({ name, slogan, description, category, default_price, original_price, features }) => {
+
+    features = features && features.map(Object.entries)
+
     return {
-      id: product_id,
-      sale: sale_price,
-      price: original_price,
-      default_price: original_price,
-      images: photos.map(p => p.url),
-      img: photos[0].url
+      name,
+      slogan,
+      description,
+      category,
+      price: default_price || original_price,
+      features
     };
   },
 
-  formatStyles: (prod) => {
-    let {results, product_id} = prod;
+  formatStyles: async ({ results, product_id }) => {
+    let formatted = {}
+    formatted.id = Number(product_id);
+    formatted.images = [];
+    formatted.thumbnails = [];
 
-    return results.map(p => {
-      let {
-        sale_price,
-        original_price,
-        photos
-      } = p;
-      return {
-        id: Number(product_id),
-        sale: sale_price, // conditionally render
-        price: original_price,
-        default_price: original_price,
-        images: photos.map(p => p.url),
-        img: photos[0].url
-      }; // extend with more data
+    await results.forEach((style) => {
+      let { sale_price, original_price, photos } = style;
+      if (sale_price) { formatted.sale = sale_price }
+      if (style['default?']) { formatted.price = original_price }
+      formatted.default_price = original_price // just in case
+      photos.forEach((p) => {
+        !formatted.images.includes(p.url) && formatted.images.push(p.url);
+        !formatted.thumbnails.includes(p.thumbnail_url) && formatted.thumbnails.push(p.thumbnail_url);
+      })
     });
+    formatted.img = formatted.images[0]
+    return formatted
   },
 
-  extendStyleToProduct: (data, style) => {
-    return $.extend(data, style);
-  }
-
+  extendStyleToProduct: (style, data) => {
+    return $.extend(style, data);
+  },
 };
 
 // Helpers for router and formatting
+
+
+// 1 - when page renders, load default cards with loading gif for related carousel
+  // 1a - check for localStorage array of outfit IDs - make API call if necessary or initialize to an empty array
+// 2 - current product is an ID, render with data as necessary
+  // 2a - memoize data for quick reload in a local object - check for {id: { product: {}, related: [{},{},{}] }}
+// 3 - load page with static data ?
+// 4 - when current product changes, update state of parent component
+
+/*
+
+this.state = {
+  currentID: 61590,
+  current: {} || null
+  relatedIDs: [62019,62121,62400,61889,61728],
+  related: [{},{},{},{},{}],
+  _storage: {
+    61590: {
+      relatedIDs: [123],
+      related: [{}]
+    }
+  },
+  outfit: [{},{}],
+  outfitIDs: [61588, 61589] // use localStorage
+}
+
+
+*/
--- server/routes/related.js
+++ server/routes/related.js
@@ -7,61 +7,63 @@ const {
   getProducts,
   getStyles,
   formatInfo,
-  // formatStyle,
   formatStyles,
 } = require('../../Shared/related.js');
 
 const parseArrayParams = (url) => {
   let path = url.split('/')
   let last = path.pop()
-  let params = last.split(',').map(Number)
-  let base = path.join('/') + '/';
-  return [base, params]
+  let params = last.split(',').map(Number) //array of arguments to forward through '/all' route
+  let prefix = path.join('/') // + '/'
+  return [prefix, params]
 }
 
 const hasArrayParams = url => url.includes(',')
 
 products.get('/', async (req, res) => { //get a list of product info
+  console.log( new URL(req.url, `http://${(req.headers.host)}`))
+  console.log(res.url, 'res')
   try {
     var list
     var products = await getProducts()
     .then(products => products.data)
-    // .then(console.log)
-    .then(results => list = Promise.all(results.map(p => formatInfo(p))))
-    .then(result => res.status(200).send(result))
+    .then(results => Promise.allSettled(results.map(p => formatInfo(p))))
+    .then(results => results.map(result => result.value))
+    .then(success => res.status(200).send(success))
     .catch(err => console.error(err))
-
   } catch (error) {
     res.status(404).send('Invalid Product ID')
   }
 });
 
-products.use('/all', async (req, res, next) => {
-  let {url} = req
-  try {
-    var valid = await hasArrayParams(url)
-    if (!valid) throw 'expected an array'
-    var array = await parseArrayParams(url)
-    let [URL, ids] = array
-    console.log(req)
-      ids.forEach(id =>
-        console.log(products)
-        // res.redirect('/products' + URL + id) // this doesn't work
-        // products.get(URL + id) // this doesn't work
-      )
+// products.use('/all', async (req, res, next) => {
+//   let {url} = req
+//   try {
+
+    // var valid = await hasArrayParams(url)
+    // if (!valid) throw 'expected an array'
+
+    // var array = await parseArrayParams(url)
+    // let [URL, ids] = array
+    // console.log(URL, 'new url', url, 'original')
 
 
-    // res.send(array)
+    /*
 
-    next()
-    res.end()
-  } catch (error) {
-    res.send(error)
-    next()
-  }
-})
+    Attempting to create a middleware/route interceptor/prefix that can accept an array of params and forward them on, for each argument. Need a way to aggregate data or reroute back to this endpoint
 
-products.get('/related/:id', async (req, res) => { //returns an array of ids
+    // res.redirect('/products' + '/' + URL + id) // this doesn't work
+    // products.get(URL + id) // this doesn't work
+  */
+
+//     next()
+//   } catch (error) {
+//     res.send(error)
+//     next()
+//   }
+// })
+
+products.get('/related/:id', async (req, res) => { // Returns an array of ids
   try {
     let {id} = req.params;
     const ids = await getRelatedIds(id)
@@ -71,23 +73,29 @@ products.get('/related/:id', async (req, res) => { //returns an array of ids
   }
 });
 
-products.get('/related/all/:id', async (req, res) => { //returns an array of aggregated styles for all ids
+products.get('/related/all/:id', async (req, res) => {
+
+  // Returns an array of aggregated styles for all ids related to the requested id
+
   try {
     let {id} = req.params;
-    console.log(req.url)
     const ids = await getRelatedIds(id)
     const styles = Promise.allSettled(ids.data.map(async i => {
       var style = await getStyles(i)
       var formatted = await formatStyles(style.data)
       return formatted
     }))
+    .then(success => success.map(result => result.value))
     .then(allRelated => res.status(200).send(allRelated))
   } catch (error) {
     res.status(404).send('Invalid Product ID')
   }
 });
 
-products.get('/info/:id', async (req, res) => { // Returns basic info for one product
+products.get('/info/:id', async (req, res) => {
+
+  // Returns basic info for one product WITH features
+
   try {
     let {id} = req.params;
     let product = await getProductInfo(id)
@@ -99,6 +107,10 @@ products.get('/info/:id', async (req, res) => { // Returns basic info for one pr
 });
 
 products.get('/styles/:id', async (req, res, next) => {
+
+  // Working well! sends an array of formatted objects, to be extended with basic data after initial render
+  // ** Does not include "features" for modal popup
+
   try {
     let {id} = req.params;
     let styles = await getStyles(id)
@@ -107,18 +119,18 @@ products.get('/styles/:id', async (req, res, next) => {
   } catch (error) {
     res.status(404).send('Invalid Product ID')
   }
-}); // Working ! sends an array of formatted objects, to be extended with basic data after initial render
+});
 
-products.get('/features/:pair', async (req, res) => { //[id1, id2].join() -> '123,456'
+
+products.get('/features/:pair', async (req, res) => { // [id1, id2].join() -> '123,456', from App
   try {
-  let ids = req.params.pair.split(',').map(Number) // pair -> [123, 456]
+  let ids = await req.params.pair.split(',').map(Number) // pair -> [123, 456]
+  .then(ids => Promise.all( ids.map(id => getProductInfo(id)) ))
+  .then(result => res.send(result))
+
   let [current, target] = ids
 
-  if (Number.isNaN(current) || Number.isNaN(current)) {
-    throw 'error'
-  }
-
-  var features = await Promise.allSettled(ids.map()) //something
+  res.status(200).send(ids)
 
   } catch (error) {
     res.status(404).send('Invalid Product ID(s)')
