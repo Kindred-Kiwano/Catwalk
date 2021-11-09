8a60b232305c0f511a19697afb97cc1b344b611f
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-11-09 00:42:30 -0600

    Investigating items to test

--- client/src/components/ProductOverview/Tests/ProductOverview.test.jsx
+++ client/src/components/ProductOverview/Tests/ProductOverview.test.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import LoadProduct from '../LoadProduct.jsx';
 
+
 describe('Basic content rendering', () => {
+
 
   test('should render text to the page', () => {
     render(<LoadProduct />);
@@ -20,9 +22,15 @@ describe('Basic content rendering', () => {
     expect(images.length).not.toBe(0);
   });
 
-  // to test:
-    // import and render <Gallery/> make sure only 7 images render to the carousel at a time (need to actually fix that)
-    // test components that take props - pass in props in the render() method to see if they appear in the expect block
 });
 
+describe('component-specific tests', () => {
+  render(<LoadProduct />);
+
+});
+
+
+  // to test:
+    // import and render <Gallery/> make sure only 7 images render to the carousel at a time (need to actually fix that)
+  // test components that take props - pass in props in the render() method to see if they appear in the expect block
 
