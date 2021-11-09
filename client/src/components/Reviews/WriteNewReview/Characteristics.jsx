74bfa20abd52d1c28df5af71e6155d3d029f0d90
David Deaton <ddeatonv@gmail.com>
2021-11-09 10:17:44 -0800

    Refactored more of WriteReview, adding more CSS

--- client/src/components/Reviews/WriteNewReview/Characteristics.jsx
+++ client/src/components/Reviews/WriteNewReview/Characteristics.jsx
@@ -1,37 +1,46 @@
 import React, { useState } from 'react';
 import '../styles/style.css';
 
-const Characteristics = ( { handleChange, messageOptions, title } ) => {
+const Characteristics = ( { handleChange, title, properTitle, messageOptions, leftWord, rightWord, middleWord } ) => {
 
   const [rating, setRating] = useState(null);
   const [message, setMessage] = useState('');
-
+  const [sentiment, setSentiment] = useState(null);
+  const [messageColor, setMessageColor] = useState(null);
+  const colorArray = ['black', 'red', 'orange', 'green', 'blue', 'purple'];
   return (
-    <div className="boddy">
+    <div className={title}>
+      <h1>{properTitle}</h1>
       <p>How would you describe the {title} of the item?</p>
-      {[...Array(5)].map((item, i) => {
-        const ratingNum = i + 1;
-        const ratingValue = ratingNum.toString();
-        return (
-          <label>
-            <input
-              name={title}
-              type="radio"
-              value={ratingValue}
-              onChange={handleChange}
-              onClick={() => {
-                setRating(ratingValue);
-                console.log(ratingValue);
-                setMessage(messageOptions[i]);
-              }}
-              checked={rating === ratingValue}
-            />
-          </label>
-        );
-      })}
-      <p>{message}</p>
+      <span> {leftWord}
+        {messageOptions.map((message, i) => {
+          const ratingNum = i + 1;
+          const ratingValue = ratingNum.toString();
+          return (
+            <label>
+              <input
+                name={title}
+                type="radio"
+                style={{'background-color': messageColor}}
+                value={ratingValue}
+                onChange={handleChange}
+                onClick={() => {
+                  setRating(ratingValue);
+                  setMessage(messageOptions[i].message);
+                  setSentiment(messageOptions[i].sentiment);
+                  setMessageColor(colorArray[messageOptions[i].sentiment]);
+                  console.log(messageColor);
+                }}
+                checked={rating === ratingValue}
+              />
+            </label>
+          );
+        })}
+        {rightWord}</span>
+      <p className="test" style={{'color': messageColor}}>{message}</p>
     </div>
   );
 };
 
 export default Characteristics;
+//<p className="test" style={{'--rating': `${sentiment}`}}>{sentiment}</p>
